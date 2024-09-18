a2b0ff5743f716420c011d31cacf74e2adf89cd9
ZoieV2025 <zoie.vitale2025@gbwl.org>
2024-09-18 13:41:55 +0000

    updated java

--- Main.java
+++ Main.java
@@ -9,28 +9,55 @@ public class Main {
     int speedKmHr = speedKmMin * 60;
     int distanceKm = 380000;
     int time = distanceKm / speedKmHr;
+    System.out.print("Question 1.: ");
     System.out.println(time);
 
 // Problem #2
    int mSecH = 2000;
    int mSecV = 3000;
-   int totalSpeedH = mSecH * mSecH;
-   int totalSpeedV = mSecV * mSecV;
-   int totalSpeedVH = totalSpeedH + totalSpeedV;
+   mSecH *= mSecH;
+   //int totalSpeedH = mSecH * mSecH;
+   mSecV *= mSecV;
+   //int totalSpeedV = mSecV * mSecV;
+   int totalSpeedVH = mSecH + mSecV;
+   //int totalSpeedVH = totalSpeedH + totalSpeedV;
    double totalSpeedChange = Math.sqrt(totalSpeedVH);
+   System.out.print("Question 2.: ");
    System.out.println(totalSpeedChange);
 
 //Problem 3
 
 //a.
-int moonRadiusK = 1731;
-int moonRadiusM = moonRadiusK * 1000;
-int moonRadiusA = moonRadiusM * moonRadiusM;
-double moonSurfaceA = 4 * 3.14 * moonRadiusA; // tmr replace 3.14 with pie
-System.out.println(moonSurfaceA);
+double moonRadiusK = 1731; // units KM
+double moonRadiusM = moonRadiusK * 1000; //converted to meters
+moonRadiusM *= moonRadiusM;
+//moonRadiusM * moonRadiusM; // squared (r^2)
+double moonSurfaceA = 4 * 3.14 * moonRadiusM;
+//double moonSurfaceA = 4 * 3.14 * moonRadiusSq; // tmr replace 3.14 with pie
+double thickness = 0.001; // m
+double moonSurfaceV = moonSurfaceA * thickness;
+System.out.print("Question 3.a: ");
+System.out.println(moonSurfaceV);
 
 //b.
+double moonDensityKg = 3000; //KG per cubic meter (m^3)
+double moonMassKg = moonDensityKg * moonSurfaceV; // solving for KG
+double moonMassMt = moonMassKg / 1000; 
+System.out.print("Question 3.b: ");
+System.out.println(moonMassMt);
 
- 
+//c.
+double volume = moonSurfaceV;
+double surfaceLayer = volume * .25; 
+double litersWater = surfaceLayer * 1000;
+System.out.print("Question 3.c: ");
+System.out.println(litersWater);
+
+
    }
 }
+
+//Grading
+// - coding style: naming variables, organizing well
+// - good java conventions, comments to help organize code and explain formulas
+// - ***using shortcut operators (1.5)
